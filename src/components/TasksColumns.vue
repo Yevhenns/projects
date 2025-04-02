2749be054b1e46fa85acd9cacf1ea87af2fd555e
Yevhenns <gifestum@gmail.com>
2025-04-02 18:20:37 +0300

    fixed create task

--- src/components/TasksColumns.vue
+++ src/components/TasksColumns.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import TasksColumnItem from './TasksColumnItem.vue'
 import { useTasksStore } from '@/stores/tasks'
 import { useProjectsStore } from '@/stores/projects'
@@ -7,19 +7,19 @@ import { useProjectsStore } from '@/stores/projects'
 const projectsStore = useProjectsStore()
 const tasksStore = useTasksStore()
 
-const list1 = tasksStore.allTasks.filter(
-  (task) => task.projectId === projectsStore.currentProject?.id,
-)
+const list1 = computed(() => {
+  return tasksStore.allTasks.filter((task) => task.projectId === projectsStore.currentProject?.id)
+})
 
 const list2 = ref(
-  list1.map((item) => ({
+  list1.value.map((item) => ({
     name: `${item.title}-2`,
     id: `${item.id}-2`,
   })) as unknown as Task[],
 )
 
 const list3 = ref(
-  list1.map((item) => ({
+  list1.value.map((item) => ({
     name: `${item.title}-3`,
     id: `${item.id}-3`,
   })) as unknown as Task[],
@@ -27,6 +27,9 @@ const list3 = ref(
 </script>
 
 <template>
+  <p>{{ tasksStore.allTasks }}</p>
+  <p>{{ projectsStore.currentProject }}</p>
+  <p>{{ list1 }}</p>
   <h2 v-if="list1.length === 0" class="heading">Немає завдань</h2>
   <div v-if="list1.length > 0">
     <h2 class="heading">Завдання</h2>
