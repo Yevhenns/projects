98f069194f146f50963011f8407fc3d0dd0a62f6
Yevhenns <gifestum@gmail.com>
2025-04-02 14:35:15 +0300

    header styles

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,9 @@ import 'vue3-toastify/dist/index.css'
 
 <template>
   <header class="header">
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-      </nav>
-    </div>
+    <nav>
+      <RouterLink class="link" to="/">Home</RouterLink>
+    </nav>
   </header>
 
   <main class="main">
@@ -19,8 +17,25 @@ import 'vue3-toastify/dist/index.css'
 
 <style scoped lang="scss">
 .header {
-  border-bottom: 1px solid black;
-  padding: 20px;
+  background-color: #333;
+  color: #fff;
+  border-bottom: 1px solid #444;
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.link {
+  color: #fff;
+  text-decoration: none;
+  font-size: 16px;
+  font-weight: 600;
+  transition: color 0.3s ease;
+
+  &:hover {
+    color: #3498db;
+  }
 }
 
 .main {
